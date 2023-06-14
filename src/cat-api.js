27cfda122cc400758d4503e763bdd88c67823414
Rammbest <igor-kutsenko@ukr.net>
2023-06-14 22:58:27 +0300

    add render breed at page

--- src/cat-api.js
+++ src/cat-api.js
@@ -18,3 +18,32 @@ export async function fetchBreeds() {
     Notiflix.Notify.warning('error');
   }
 }
+
+
+export async function fetchCatByBreed(breedId) {
+
+  try {
+    const response = await axios.get(`${BASE_URL}images/search?breed_ids=${breedId}&api_key=${api_key}`);
+    const data = response.data;
+    if (data.length > 0) {
+      const cat = data[0];
+      const breed = cat.breeds[0];
+      return {
+        name: breed.name,
+        description: breed.description,
+        temperament: breed.temperament,
+        origin: breed.origin,
+        life_span: breed.life_span,
+        image: cat.url
+      };
+    }
+
+   
+
+  } catch (error) {
+
+    console.log(error);
+    Notiflix.Notify.warning('error');
+  }
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fetchBreeds } from './cat-api';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 import Notiflix from 'notiflix';
 import axios from "axios";
@@ -16,8 +16,38 @@ function renderBreedsSelect(breeds) {
   breedSelectRef.innerHTML = breeds
     .map(breed => `<option value="${breed.id}">${breed.name}</option>`)
     .join('');
-  // breedSelectRef.addEventListener('change', handleBreedSelectChange);
+  breedSelectRef.addEventListener('change', showCatByBreed);
 }
 
 showBreeds();
 
+
+
+async function showCatByBreed(event) {
+  const selectedBreedId = event.target.value;
+  const cat = await fetchCatByBreed(selectedBreedId);
+  renderCatInfo( cat);
+  
+}
+
+
+function renderCatInfo(cat) {
+  const {
+    name,
+    image,
+    description,
+    temperament,
+    origin,
+    life_span
+  } = cat;
+
+  catInfoRef.innerHTML = `
+    <img class="cat-img" src="${image}" alt="${name}">
+    <div class="cat-text">
+      <h2 class="cat-header">${name}</h2>
+      <p>${description}</p>
+      <p><span><b>Temperament: </b></span>${temperament}</p>
+      <p><span><b>Origin: </b></span>${origin}</p>
+      <p><span><b>Life Span: </b></span>${life_span}</p>
+    </div>`;
+}
