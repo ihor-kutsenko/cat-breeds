5649261aa5fba75d7bef2340134e2e9749c48836
Rammbest <igor-kutsenko@ukr.net>
2023-06-14 23:45:16 +0300

    add error mesage. final version

--- src/index.js
+++ src/index.js
@@ -6,11 +6,39 @@ import axios from "axios";
 const breedSelectRef = document.querySelector('.breed-select');
 const catInfoRef = document.querySelector('.cat-info');
 const loaderRef = document.querySelector('.loader');
+const errorRef = document.querySelector('.error');
+
+loaderRef.classList.add('hidden');
+catInfoRef.classList.add('hidden');
+errorRef.classList.add('hidden'); 
+
 
 async function showBreeds() {
-  const breeds = await fetchBreeds();
-  renderBreedsSelect(breeds);
+  hideCatInfo();
+  
+  try {
+    const breeds = await fetchBreeds();
+    renderBreedsSelect(breeds);
+      loaderRef.classList.add('hidden');
+
+    
+    if (breeds.length > 0) {
+        selectedBreedId = breeds[0].id;
+        
+      } else {
+        
+        showError('No breeds found.');
+    }
+    
+  } catch (error) {
+    loaderRef.classList.add('hidden');
+      errorNotiflix();
+    
+  }
+  
 }
+
+
 
 function renderBreedsSelect(breeds) {
   breedSelectRef.innerHTML = breeds
@@ -20,13 +48,40 @@ function renderBreedsSelect(breeds) {
 }
 
 showBreeds();
+hideCatInfo()
 
 
 
 async function showCatByBreed(event) {
-  const selectedBreedId = event.target.value;
-  const cat = await fetchCatByBreed(selectedBreedId);
-  renderCatInfo( cat);
+  hideCatInfo();
+  try {
+const selectedBreedId = event.target.value;
+    const cat = await fetchCatByBreed(selectedBreedId);
+    if (cat) {
+        renderCatInfo(cat);
+      catInfoRef.classList.remove('hidden');
+      loaderRef.classList.add('hidden');
+      
+      } else {
+        
+        errorRef.classList.remove('hidden');
+      }
+      
+    
+
+    
+    
+
+    if (cat.length === 0) {
+      errorRef.classList.remove('hidden');
+      
+    }
+    
+  } catch (error) {
+    loaderRef.classList.add('hidden');
+      errorNotiflix();
+  }
+  
   
 }
 
@@ -50,4 +105,31 @@ function renderCatInfo(cat) {
       <p><span><b>Origin: </b></span>${origin}</p>
       <p><span><b>Life Span: </b></span>${life_span}</p>
     </div>`;
+}
+
+
+
+function hideCatInfo() {
+  if (!catInfoRef.classList.contains('hidden')) {
+    catInfoRef.classList.add('hidden');
+  }
+  loaderRef.classList.remove('hidden');
+  errorRef.classList.add('hidden'); 
+}
+
+
+
+function showError(message) {
+  errorRef.textContent = message;
+  errorRef.classList.remove('hidden');
+}
+
+function errorNotiflix() {
+  Notiflix.Notify.failure(
+    `Oops! Something went wrong! Try reloading the page!`,
+    {
+      clickToClose: true,
+      timeout: 4000,
+    }
+  );
 }
